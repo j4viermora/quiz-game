1dd0a121b168c8d2475933fef9396b1ca8dd5a87
Javier Alfonso Mora gil <55900303+j4viermora@users.noreply.github.com>
2021-12-07 18:21:35 -0400

    new handle options

--- src/components/SelectDifficultty.js
+++ src/components/SelectDifficultty.js
@@ -1,18 +1,29 @@
 import { setDifficulty } from "actions/quiz";
 import { useDispatch } from "react-redux";
 
+const options = [
+    { value: "easy", display: "Easy" },
+    { value: "medium", display: "Medium" },
+    { value: "hard", display: "Hard" },
+];
+
 export default function SelectDifficultty({ className }) {
     const dispatch = useDispatch();
 
+    const handleChange = ({ target }) => {
+        if (target.value !== "no-selected")
+            dispatch(setDifficulty(target.value));
+    };
+
     return (
-        <div
-            className={`select ${className} `}
-            onChange={(e) => dispatch(setDifficulty(e.target.value))}
-        >
+        <div className={`select ${className} `} onChange={handleChange}>
             <select>
-                <option value="easy">Easy</option>
-                <option value="medium">Medium</option>
-                <option value="hard">Hard</option>
+                <option value="no-selected">Select Difficulty</option>
+                {options.map(({ value, display }) => (
+                    <option key={value} value={value}>
+                        {display}
+                    </option>
+                ))}
             </select>
         </div>
     );
