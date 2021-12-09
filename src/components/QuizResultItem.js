4074fdb9b851f466a5c87fc05246436e4c7e13c6
Javier Alfonso Mora gil <55900303+j4viermora@users.noreply.github.com>
2021-12-09 19:53:11 -0400

    Redundancy in the test result item component is removed

--- src/components/QuizResultItem.js
+++ src/components/QuizResultItem.js
@@ -1,26 +1,22 @@
-import React from "react";
 import { AiFillCloseCircle, AiFillCheckCircle } from "react-icons/ai";
-import { colors } from "styles/theme";
 import htmlparse from "react-html-parser";
+
+// Personal imports
+import { colors } from "styles/theme";
 
 export default function QuizResultItem({ question, isCorrect }) {
     return (
         <>
-            {isCorrect ? (
-                <div className="is-flex">
-                    <span className="icon mr-1">
+            <div className="is-flex">
+                <span className="icon mr-1">
+                    {isCorrect ? (
                         <AiFillCheckCircle color={colors.primary} />
-                    </span>
-                    <p>{htmlparse(question.trim())}</p>
-                </div>
-            ) : (
-                <div className="is-flex">
-                    <span className="icon mr-1">
+                    ) : (
                         <AiFillCloseCircle color={colors.danger} />
-                    </span>
-                    <p>{question}</p>
-                </div>
-            )}
+                    )}
+                </span>
+                <p>{htmlparse(question.trim())}</p>
+            </div>
             <hr />
         </>
     );
